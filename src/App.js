a9f7c4b8a38520ecc757d374a14276a1cc23bcf1
koffidotse <koffidodotse>
2020-10-15 21:21:16 +0100

    lab commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Content } from './components/content';
+import { Footer } from './components/footer';
+import { Header } from './components/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar,Nav} from 'react-bootstrap';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+export class App extends React.Component{
+    render(){
+         return(
+           <Router>
+             <div className ="App">
+             <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/read">Read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+    </Nav>
+    </Navbar>
+               <Route path='/'component = {Content} exact/>
+               <Route path='/create'component ={Header} exact/>
+               <Route path='/read'component ={Footer} exact/>
+             </div>
+             </Router>
+         );
+
+    }
+}
+   
+export default App;
